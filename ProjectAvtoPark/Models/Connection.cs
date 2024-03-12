442eee9facf40543f6597dd13816118bd79d9ecb
Сергей Ноходоев <nohodoevS@cloud.biik.ru>
2024-03-12 08:39:52 +0800

    Подключил базу данных

---
+++ ProjectAvtoPark/Models/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectAvtoPark.Models;
+
+namespace ProjectAvtoPark.Models
+{
+    public class Connection
+    {
+        public AvtoParkEntities auth = new AvtoParkEntities();
+    }
+}
