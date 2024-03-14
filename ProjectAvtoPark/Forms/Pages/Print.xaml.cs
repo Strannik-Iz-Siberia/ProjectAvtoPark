ec57807990f5adc5d1f8f6b27b36de9e3c248ce6
Сергей Ноходоев <nohodoevS@cloud.biik.ru>
2024-03-14 13:37:22 +0800

    Реализовал возможность печати

--- ProjectAvtoPark/Forms/Pages/Print.xaml.cs
+++ ProjectAvtoPark/Forms/Pages/Print.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ProjectAvtoPark.Models;
+﻿using ProjectAvtoPark.Forms.Controls;
+using ProjectAvtoPark.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,22 +26,58 @@ namespace ProjectAvtoPark.Forms.Pages
         public Print()
         {
             InitializeComponent();
+
             var query = from arenda in AvtoParkEntities1.GetContext().View_Аренды
                         join car in AvtoParkEntities1.GetContext().Автомобиль on arenda.id_автомобиля equals car.id_автомобиля
                         join model in AvtoParkEntities1.GetContext().Модель_авто on car.id_Модель equals model.id_Модель
                         join tariff in AvtoParkEntities1.GetContext().Тарифы on arenda.id_тарифа equals tariff.id_тарифа
-                        select new
+                        select new RentalInfo
                         {
-                            Номер = car.id_автомобиля,
-                            Автомобиль = model.Название,
-                            Статус_автомобиля = car.статуса_автомобиля,
-                          
-                            Итоговая_цена = arenda.стоимость + (tariff.цена_за_час * 1) + (tariff.цена_за_сутки * 31)
+                            CarId = car.id_автомобиля,
+                            CarNumber = model.Название,
+                            Cost = arenda.стоимость + (tariff.цена_за_час * 1) + (tariff.цена_за_сутки * 31),
+                            Kolvo = 97,
+                            Kolvonearend = 3,
+                            k = 100
+
                         };
+
+
             Otchet.ItemsSource = query.ToList();
 
+        }
 
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.IsEnabled = false;
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    FixedDocument document = new FixedDocument();
+                    // Создаем экземпляр UserControl, являющийся шаблоном для одной страницы для печати
+                    UserControlPrint printTemplate = new UserControlPrint(); // Замените на вашу разметку
+                                                                             // Заполняем шаблон данными из первого объекта RentalInfo
+                    ContentControl content = new ContentControl();
+                    content.Content = printTemplate;
+                    content.DataContext = Otchet.ItemsSource; // Устанавливаем DataContext для ContentControl
+
+                    // Создаем и добавляем PageContent в документ FixedDocument
+                    PageContent pageContent = new PageContent();
+                    FixedPage fixedPage = new FixedPage();
+                    fixedPage.Children.Add(content);
+                    ((IAddChild)pageContent).AddChild(fixedPage);
+                    document.Pages.Add(pageContent);
+
+                    printDialog.PrintDocument(document.DocumentPaginator, "Имя документа");
+                }
+            }
+            finally
+            {
+                this.IsEnabled = true;
+            }
         }
     }
 }
---
+++ ProjectAvtoPark/Models/RentalInfo.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAvtoPark.Models
+{
+    public class RentalInfo : INotifyPropertyChanged
+    {
+
+
+        private int _carId;
+        public int CarId
+        {
+            get { return _carId; }
+            set { _carId = value; OnPropertyChanged("CarId"); }
+        }
+
+
+        private int _ArendaId;
+        public int ArendaId
+        {
+            get { return _ArendaId; }
+            set { _ArendaId = value; OnPropertyChanged("ArendaId"); }
+        }
+
+
+        private string _carNumber;
+        public string CarNumber
+        {
+            get { return _carNumber; }
+            set { _carNumber = value; OnPropertyChanged("CarNumber"); }
+        }
+
+        private string _carName;
+        public string CarName
+        {
+            get { return _carName; }
+            set { _carNumber = value; OnPropertyChanged("CardName"); }
+        }
+
+        private int? _modelId;
+
+        public int? ModelId
+        {
+            get { return _modelId; }
+            set { _modelId = value; OnPropertyChanged("ModelId"); }
+        }
+
+        private bool? _carStatus;
+        public bool? CarStatus
+        {
+            get { return _carStatus; }
+            set { _carStatus = value; OnPropertyChanged("CarStatus"); }
+        }
+
+        private int _rentId;
+        public int RentId
+        {
+            get { return _rentId; }
+            set { _rentId = value; OnPropertyChanged("RentId"); }
+        }
+
+        private int? _userId;
+        public int? UserId
+        {
+            get { return _userId; }
+            set { _userId = value; OnPropertyChanged("UserId"); }
+        }
+
+        private int? _tariffId;
+        public int? TariffId
+        {
+            get { return _tariffId; }
+            set { _tariffId = value; OnPropertyChanged("TariffId"); }
+        }
+
+        private int? _cost;
+        public int? Cost
+        {
+            get { return _cost; }
+            set
+            {
+                _cost = value;
+                OnPropertyChanged("Cost");
+            }
+        }
+
+        private int? _kolvo;
+        public int? Kolvo
+        {
+            get { return _kolvo; }
+            set
+            {
+                _kolvo = value;
+                OnPropertyChanged("Kolvo");
+            }
+        }
+
+        private int? _kolvonearend;
+        public int? Kolvonearend
+        {
+            get { return _kolvonearend; }
+            set
+            {
+                _kolvonearend = value;
+                OnPropertyChanged("Kolvonearend");
+           
+           }
+        }
+
+        private int? _k;
+        public int? k
+        {
+            get { return _k; }
+            set
+            {
+                _k = value;
+                OnPropertyChanged("k");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
